6cc24c2ae4bd994b36ec9e226aaac01f2ac28de9
fortran <qnev89@gmail.com>
2016-03-07 23:06:04 +0200

    Adding of the first init method

---
+++ EchoCommander.h
@@ -0,0 +1,19 @@
+/*
+ * EchoCommander.h
+ *
+ *  Created on: Mar 7, 2016
+ *      Author: iqnev
+ */
+
+#ifndef ECHOCOMMANDER_H_
+#define ECHOCOMMANDER_H_
+
+class EchoCommander {
+ public:
+  EchoCommander();
+  virtual ~EchoCommander();
+ private:
+  void init();
+};
+
+#endif /* ECHOCOMMANDER_H_ */
